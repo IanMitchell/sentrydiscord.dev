d40d5c5a488e76e6eed02d0052c4c155cbfb4c3a
Nickyux <me@nickyux.com>
2025-07-03 12:26:56 +0200

    fix(index): remove persisting warning icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -103,21 +103,6 @@ export default function Home({ events, webhooks }) {
 						</div>
 					</div>
 
-					<div className="rounded-md bg-red-50 p-4 max-w-4xl mx-auto mb-8">
-						<div className="flex">
-							<div className="flex-shrink-0">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									className="h-5 w-5 text-red-400"
-									aria-hidden="true"
-									viewBox="0 0 16 16"
-								>
-									<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
-								</svg>
-							</div>
-						</div>
-					</div>
-
 					<DonationBanner />
 
 					<div className="max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
